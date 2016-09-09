8522d68544c841172d7e4b9192717d81466207ad
Matteo Croce <matteo.croce@canonical.com>
2016-09-09 17:06:06 +0200

    smaller httpd, argument change

--- httpd.go
+++ httpd.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	. "github.com/mattn/go-getopt"
+	"github.com/mattn/go-getopt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
+
+var root, _ = os.Getwd()
 
 func sizeToByte(size int64) string {
 	const suffixes string = " KMGT"
@@ -23,41 +25,40 @@ func sizeToByte(size int64) string {
 }
 
 func listDir(resp http.ResponseWriter, req *http.Request, path string) {
-	files, _ := ioutil.ReadDir(path)
-	io.WriteString(resp, "<!DOCTYPE html>\n<html><head><title>" + path + "</title></head><body><table>\n")
-	if path != "." {
+	files, _ := ioutil.ReadDir(root + path)
+	io.WriteString(resp, "<!DOCTYPE html>\n<html><head><title>"+path+"</title></head><body><table style='font-family: monospace;'>\n")
+	if path == "/" {
+		path = ""
+	} else {
 		io.WriteString(resp, "<tr><td colspan=\"2\"></td><td><a href=\"..\">../</a></td></tr>\n")
 	}
 	for _, file := range files {
 		name := file.Name()
 		if file.Mode()&os.ModeSymlink != 0 {
-			link, _ := os.Readlink(path + "/" + name)
+			link, _ := os.Readlink(root + path + "/" + name)
 			name += " -> " + link
 		}
-		res := "<td>" + file.Mode().String() + "</td><td>" + sizeToByte(file.Size()) + "</td>"
-		res += `<td><a href="/` + path + "/" + file.Name() + `">` + name + "</a></td>"
-		io.WriteString(resp, "<tr>"+res+"</tr>\n")
+		io.WriteString(resp,
+			"<tr><td>"+file.Mode().String()+"</td><td>"+sizeToByte(file.Size())+
+				`</td><td><a href="`+path+"/"+file.Name()+`">`+name+"</a></td></tr>")
 	}
 	io.WriteString(resp, "</table></body></html>")
 }
 
 func serveStatic(resp http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path[1:]
-	if len(path) == 0 {
-		path = "."
-	}
+	path := req.URL.Path
 
-	if fi, err := os.Stat(path); os.IsNotExist(err) {
+	if fi, err := os.Stat(root + path); os.IsNotExist(err) {
 		resp.WriteHeader(http.StatusNotFound)
 		io.WriteString(resp, path+": "+http.StatusText(http.StatusNotFound))
 	} else {
-		if file, err := os.Open(path); err == nil {
+		if file, err := os.Open(root + path); err == nil {
 			if fi.IsDir() {
 				listDir(resp, req, path)
 			} else {
 				io.Copy(resp, file)
-				file.Close()
 			}
+			file.Close()
 		} else if os.IsPermission(err) {
 			resp.WriteHeader(http.StatusForbidden)
 			io.WriteString(resp, path+": "+http.StatusText(http.StatusForbidden))
@@ -66,26 +67,19 @@ func serveStatic(resp http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	var port string = "80"
-	var root string
-	var c int
-	OptErr = 0
-	for {
-		if c = Getopt("p:r:h"); c == EOF {
-			break
-		}
+	var address string = ":80"
+	for c := 0; c != getopt.EOF; c = getopt.Getopt("b:r:h") {
 		switch c {
-		case 'p':
-			port = OptArg
+		case 'b':
+			address = getopt.OptArg
 		case 'r':
-			root = OptArg
+			root = getopt.OptArg
 		case 'h':
-			log.Printf("usage: httpd -p port -r root")
+			log.Printf("usage: httpd [-b address:port] [-r root]")
 			os.Exit(0)
 		}
 	}
 
-	os.Chdir(root)
 	http.HandleFunc("/", serveStatic)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(address, nil))
 }
